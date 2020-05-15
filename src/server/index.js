2db9b7466840638fc5036ae6e065a0665aea2fef
bryanpan342 <bryanpan342@gmail.com>
2020-05-15 03:30:41 +0000

    removed old REST code

--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,3 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-// const db = require('./queries')
-// const port = 3000
-
-// app.use(bodyParser.json())
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// )
-
-// app.get('/', (request, response) => {
-//   response.send({ info: 'Test, Node.js, Express, and Postgres API' })
-// })
-
-// app.get('/data', db.getData)
-// app.post('/data', db.pushData)
-
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
-
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
